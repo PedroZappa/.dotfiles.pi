34c36303e14d53bb2d7d1ae6c3ee03f5cacc3010
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-01-01 21:14:45 +0000

    scripts/minenv.sh : updated

--- scripts/minenv.sh
+++ scripts/minenv.sh
@@ -80,6 +80,38 @@ install_snap_package() {
 # **************************************************************************** #
 # **************************************************************************** #
 
+DOTFILES_SSH_URL="git@github.com:PedroZappa/dotfiles.min.git"
+
+# Associative array defining source and target FILES
+declare -A FILES
+FILES=(
+    ["$HOME/.dotfiles/.gitconfig"]="$HOME/.gitconfig"
+    ["$HOME/.dotfiles/.zshrc"]="$HOME/.zshrc"
+    ["$HOME/.dotfiles/.zshenv"]="$HOME/.zshenv"
+    ["$HOME/.dotfiles/.gdbinit"]="$HOME/.gdbinit"
+    ["$HOME/.dotfiles/.vimrc"]="$HOME/.vimrc"
+    ["$HOME/.dotfiles/.tmux.conf"]="$HOME/.tmux.conf"
+    ["$HOME/.dotfiles/btop/"]="$HOME/.config/"
+	["$HOME/.dotfiles/starship.toml"]="$HOME/.config/"
+	["$HOME/.dotfiles/nvim"]="$HOME/.config/"
+)
+
+# Create symlinks to .dotfiles
+create_symlink() {
+    local SRC=$1
+    local DEST=$2
+
+    # Create the parent directory if it doesn't exist
+    mkdir -p "$(dirname "$DEST")"
+    # Create the symlink
+    ln -s "$SRC" "$DEST"
+    echo "${YEL}Created symlink from ${GRN}$SRC ${YEL}to ${PRP}$DEST${D}"
+}
+#
+# **************************************************************************** #
+#                                 GO BASH GO!                                  #
+# **************************************************************************** #
+
 # Create SSH key and confirm with the user
 create_ssh_key
 
@@ -112,45 +144,11 @@ sudo apt clean
 
 # Install snap packages
 echo "${MAG}Installing snap packages...${D}"
-install_snap_packages "${snap_packages[@]}"
+install_snap_package "${snap_packages[@]}"
 
 # Install Python libraries using pip
 # echo "Installing Python libraries..."
 # pip3 install --user ${PACKAGES["python_libraries"]}
-
-# **************************************************************************** #
-# **************************************************************************** #
-
-DOTFILES_SSH_URL="git@github.com:PedroZappa/dotfiles.min.git"
-
-# Associative array defining source and target FILES
-declare -A FILES
-FILES=(
-    ["$HOME/.dotfiles/.gitconfig"]="$HOME/.gitconfig"
-    ["$HOME/.dotfiles/.zshrc"]="$HOME/.zshrc"
-    ["$HOME/.dotfiles/.zshenv"]="$HOME/.zshenv"
-    ["$HOME/.dotfiles/.gdbinit"]="$HOME/.gdbinit"
-    ["$HOME/.dotfiles/.vimrc"]="$HOME/.vimrc"
-    ["$HOME/.dotfiles/.tmux.conf"]="$HOME/.tmux.conf"
-    ["$HOME/.dotfiles/btop/"]="$HOME/.config/"
-	["$HOME/.dotfiles/starship.toml"]="$HOME/.config/"
-	["$HOME/.dotfiles/nvim"]="$HOME/.config/"
-)
-
-# Create symlinks to .dotfiles
-create_symlink() {
-    local SRC=$1
-    local DEST=$2
-
-    # Create the parent directory if it doesn't exist
-    mkdir -p "$(dirname "$DEST")"
-    # Create the symlink
-    ln -s "$SRC" "$DEST"
-    echo "${YEL}Created symlink from ${GRN}$SRC ${YEL}to ${PRP}$DEST${D}"
-}
-
-# **************************************************************************** #
-# **************************************************************************** #
 
 if [ ! -d "$HOME/.dotfiles" ]; then
     # Clone the dotfiles repository
