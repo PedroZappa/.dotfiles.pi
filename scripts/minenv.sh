c5eb2fd342625de5aed6f47f9e3f162dc8e3026d
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-01-01 21:39:43 +0000

    scripts/minenv.sh : updated

--- scripts/minenv.sh
+++ scripts/minenv.sh
@@ -75,7 +75,7 @@ install_zap() {
 # Define package categories in separate variables for better maintainability
 my_shell=("/usr/bin/zsh")
 core_tools=("build-essential" "cmake" "g++" "make" "git" "tmux" "curl" "wget" "vim" "clang" "valgrind" "gdb" "libssl-dev" "libboost-all-dev" "ninja-build" "googletest")
-additional_tools=("snapd" "btop" "tree" "ripgrep" "ncdu" "fzf")
+additional_tools=("snapd" "luarocks" "btop" "tree" "ripgrep" "ncdu" "fzf")
 snap_packages=("nvim --classic")
 # python_libraries=("numpy" "scipy" "soundfile" "pyserial")
 
