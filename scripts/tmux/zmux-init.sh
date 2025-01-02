049620fa80ca4e0c71c1dda664b64089e4510c8e
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-01-02 17:19:33 +0000

    scripts/tmux-init.sh : cleanup

--- scripts/tmux/zmux-init.sh
+++ scripts/tmux/zmux-init.sh
@@ -4,31 +4,11 @@
 # -u : Treat unset variables as an error and exit;
 # -o pipeline : Set the exit status to the last command in the pipeline that failed.
 
-# Color Codes
-# Run the following command to get list of available colors
-# bash -c 'for c in {0..255}; do tput setaf $c; tput setaf $c | cat -v; echo =$c; done'
-
 # Load Colors
 source ~/.dotfiles/scripts/colors.sh
 
 # Initialization message
 echo ${YEL}ZMUX${D}${PRP}: Initializing Dev Env...${D} ${GRN}${D}
-
-# Set Path to Obsidian Vault
-if [[ $USER == "zedr0" ]]; then			# DEV-Desk
-	OBSIDIAN_VAULT_PATH="$HOME/Documents/Obsidian/ZedroVault"
-elif [[ $USER == "passunca" ]]; then	# 42
-	OBSIDIAN_VAULT_PATH="$HOME/sgoinfre/Zedro-Vault"
-elif [[ $USER == "zedro" ]]; then		# DEV-Mac
-	OBSIDIAN_VAULT_PATH="$HOME/Library/Mobile Documents/iCloud~md~obsidian/Documents/ZedroVault"
-else
-	echo "ZMUX: Unknown user... you shall not PATH! 😅"
-fi
-
-export OBSIDIAN_VAULT_PATH
-
-# I3SOCK=$(ls /run/user/1000/i3/ipc-socket.*)
-# export I3SOCK
 
 # Command line argument for working directory
 if [[ $# -gt 0 ]]; then
@@ -68,18 +48,6 @@ tmux new-session	-d -s $SESH2
 tmux rename-window	-t DEV:1 "$PROJECT_NAME"
 tmux send-keys		-t DEV:1 'cd '$DEV_DIR C-m
 tmux send-keys		-t DEV:1 '' C-m
-# Create Debug window
-tmux new-window		-t DEV:2 -n 'GDB'
-tmux split-window	-t DEV:2 -h
-tmux send-keys		-t DEV:2 'cd '$DEV_DIR C-m
-tmux send-keys		-t DEV:2.1 'cd '$DEV_DIR C-m
-tmux send-keys		-t DEV:2.1 $EDITOR C-m
-tmux send-keys		-t DEV:2.1 ":e .vgdbinit" C-m
-tmux send-keys		-t DEV:2.1 ":split .gdbinit" C-m
-tmux resize-pane	-L 160
-# Create SYNC window
-tmux new-window		-t DEV:3 -n 'DAP'
-tmux send-keys		-t DEV:3 'cd '$DEV_DIR C-m
 
 # Attach to DEV session
 tmux attach-session -t DEV:1
